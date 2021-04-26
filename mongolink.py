91e9893ca0a11b49e82ed81c1c87a9cc93b07ce4
jackkeekhoo <jackkeekhoo@yahoo.com>
2021-04-26 14:43:38 +0800

    Update mongolink.py

--- mongolink.py
+++ mongolink.py
@@ -1,5 +1,5 @@
 #26April2021
-#10.45am
+#2.41pm
 
 import pymongo
 
@@ -16,13 +16,20 @@ class LivestockDb:
         print(self.myclient.list_database_names())
         return()
     def insertvalue(self,factory_id, serial_number, weight):
+        self.connectmongo
         mydict = { "factory id": factory_id, "serial number": serial_number, "weight": weight }
         x = self.mytable.insert_one(mydict)
         return(x)
+    def exportdata():
+        
+
+#testing
     
-#mongodatabase=LivestockDb()      
-#mytable = mongodatabase.connectmongo()
-#mongodatabase.insertvalue("11100","2701","410")
+mongodatabase=LivestockDb()      
+mytable = mongodatabase.connectmongo()
+mongodatabase.insertvalue("11100","2701","410")
+
+#export data to csv file,
 
 
 
